ef5e676cde7be9cdb488ec0d1c932bd8d57a97b7
lerato-s <sikhumbanal@gmail.com>
2026-09-11 23:58:54 +0200

    test: polyfill IndexedDB and structuredClone for offline caching tests

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,2 +1,7 @@
+if (typeof structuredClone === "undefined") {
+    global.structuredClone = require("node:util").structuredClone ?? ((obj) => JSON.parse(JSON.stringify(obj)));
+}
+
+import "fake-indexeddb/auto";
 import "@testing-library/jest-dom";
 
